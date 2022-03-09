be4fa5375fdc2a925c3134c7084636ad9ae993f5
jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-03-09 17:39:56 +0900

    Create Even_Odd.java

---
+++ Programmers/Level1/Even_Odd.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int num) {
+        
+        String answer = "";
+        if (num%2==0){
+            answer = "Even";
+        }
+        else
+            answer = "Odd";
+        return answer;
+    }
+}
+
+//프로그래머스 홀수,찍수
