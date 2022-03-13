05c35431e36633e1a2502e3c6cd7e6f21312bee5
jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-03-14 02:13:17 +0900

    Update Add_digits.java

--- Programmers/Level1/Add_digits.java
+++ Programmers/Level1/Add_digits.java
@@ -1,20 +1,14 @@
-import java.util.*;
-
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-
-        String[] a = new String[10];
-
-        String str_n = String.valueOf(n);
-        String[] strArray = str_n.split("");
-
-        for(String s : strArray) {
-            int sum  = Integer.parseInt(s);
-            answer+=sum;
-        }
-        return answer;
+		String str_n = String.valueOf(n);
+		String[] strArray = str_n.split("");
+		
+		for(String s : strArray) {
+			int sum  = Integer.parseInt(s);
+			answer+=sum;
+		}
+		return answer;
     }
 }
-
 // 프로그래머스 자릿수 더하기
