efce85369308cc7382d9665f2b834304e39357a3
jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-03-14 02:08:30 +0900

    Create Add_digits.java

---
+++ Programmers/Level1/Add_digits.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        String[] a = new String[10];
+
+        String str_n = String.valueOf(n);
+        String[] strArray = str_n.split("");
+
+        for(String s : strArray) {
+            int sum  = Integer.parseInt(s);
+            answer+=sum;
+        }
+        return answer;
+    }
+}
+
+// 프로그래머스 자릿수 더하기
