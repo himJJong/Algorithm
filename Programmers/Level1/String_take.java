e93f56bebbb69404f16dd4cbbe2e920d34b98771
jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-03-31 11:11:23 +0900

    Update String_take.java

--- Programmers/Level1/String_take.java
+++ Programmers/Level1/String_take.java
@@ -1,27 +1,23 @@
-class Solution {
-  public boolean solution(String s) {
-      if(s.length() == 4 || s.length() == 6){
-          try{
-              int x = Integer.parseInt(s);
-              return true;
-          } catch(NumberFormatException e){
-              return false;
-          }
-      }
-      else return false;
-  }
-}
-
+//프로그래머스 문자열 다루기
 
 class Solution {
     public boolean solution(String s) {
-        if(s.length() != 4 && s.length() != 6) return false;
-        for (int i=0;i<s.length();i++) {
-            if (s.charAt(i) < '0' || s.charAt(i) > '9') return false;
-        }        
-        return true;
+
+        boolean answer = true;
+        if(s.length()==4 || s.length()==6) {
+            try {
+                Integer.parseInt(s);
+            }catch(NumberFormatException e) {
+                answer = false;
+            }
+        }
+        else {
+            answer =false;
+        }
+
+        return answer;
+
     }
 }
 
-//프로그래머스 문자열 다루기
-// 참고 : https://velog.io/@dhk22/프로그래머스-문자열-다루기-기본-JAVA
+
