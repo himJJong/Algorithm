5a38b94d2ae53343fa0ab961f368b5288e77b7b2
Jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-05-01 00:30:18 +0900

    Update dog_cat.sql

--- Programmers_Mysql/Level2/dog_cat.sql
+++ Programmers_Mysql/Level2/dog_cat.sql
@@ -1,1 +1,5 @@
-~
+SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE)   뽑고자 하는 것 select, 뽑고자 하는것에 개수세기(count), 어디서부터(from)
+FROM ANIMAL_INS                            
+group by ANIMAL_TYPE                     뽑고자 하는 것 그룹으로 묶기
+ORDER BY ANIMAL_TYPE                     순서대로 정렬
+
