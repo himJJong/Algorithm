c5145005acd557f6f4b504e9b22fd54f88049d0f
Jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-05-10 20:53:57 +0900

    Update kakao_pickdoll.java

--- Programmers/Level1_internship/kakao_pickdoll.java
+++ Programmers/Level1_internship/kakao_pickdoll.java
@@ -1,1 +1,27 @@
-~
+import java.util.*;
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for(int move : moves){
+            for(int i=0; i<board.length;i++){
+                if(board[i][move-1]==0){
+                    continue;
+                }
+                else{
+                    if(!stack.isEmpty()&&board[i][move-1]==stack.peek()){
+                        answer+=2;
+                        stack.pop();
+                    }
+                    else{
+                        stack.add(board[i][move-1]);
+                    }
+                    board[i][move-1]=0;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
