a94060d895c829d956afb986539603cc4c9e8e3d
Jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-05-15 20:26:35 +0900

    Update fail_percent.java

--- Programmers/Level1_internship/fail_percent.java
+++ Programmers/Level1_internship/fail_percent.java
@@ -12,12 +12,12 @@ public class Test {
 		int[] stages = {2,1,2,6,2,4,3,3};
 		
 		int[] check = new int[N];			// 같은 배열 체크 
-		double[] rate = new double[N];		// 실패율 값 배
+		double[] rate = new double[N];			// 실패율 값 배
 		int[] answer = new int[N];			// 결과 값 배열
 		Arrays.sort(stages);
 		
 		//같은 배열요소 개수 
-    //스테이지 클리어 한거는 생각x , i번째 배열은 i+1스테이지
+   		//스테이지 클리어 한거는 생각x , i번째 배열은 i+1스테이지
 		for(int i=0; i<N;i++) {
 			for(int j=0; j<stages.length;j++) {
 				if (stages[j]==i+1) {
@@ -37,7 +37,7 @@ public class Test {
 				rate[i]=(double) numerator/denominator;   // 실패율이 정수로 안나누어 떨어지기에 double
 				System.out.println(rate[i]);
 			}
-			denominator -= check[i];                    // 전체 확률에서, 앞선 check[i] 스테이지 빼기
+			denominator -= check[i];                    	 // 전체 확률에서, 앞선 check[i] 스테이지 빼기
 		}
 		
 		//결과
@@ -49,12 +49,12 @@ public class Test {
 			target = 1;
 			for(int j=0; j<rate.length;j++) {
 				if(rate[j]>max) {                        // 큰값 비교		
-					max=rate[j];                           // 인데스 보다 +1인 스테이지 갑 정의
+					max=rate[j];                     // 인데스 보다 +1인 스테이지 갑 정의
 					target = j+1;
 				}
 			}
-			rate[target-1] = -1;                       // 체크한 실패율 -1 초기화
-			answer[i] = target;                        // 실패율 큰값부터 추가
+			rate[target-1] = -1;                       	// 체크한 실패율 -1 초기화
+			answer[i] = target;                         	// 실패율 큰값부터 추가
 		}
 		
 	}
