5179ef9345a46483bd646cda922f130f9b69f483
Jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-05-31 15:03:26 +0900

    Create 1439.java

---
+++ Baekjoon/Greedy/1439.java
@@ -0,0 +1,59 @@
+//for, if문으로 짜는 코드는 한계가 있다. 이제 다르게 생각하고, 아래 방법은 지양하도록 하자.
+package algorithm;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Test {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String n = st.nextToken();
+		String[] array = n.split("");
+		int zerotoone = 0;		//0 -> 1로  
+		int onetozero = 0;		//1 -> 0
+		int result = 0;
+		
+		
+		for(int i=0; i<array.length-1 ;i++) {
+			if(array[i].equals("0") && array[i+1].equals("1")) {
+				zerotoone++;
+			}
+			else if(array[i].equals("0") && array[i+1].equals("0")) {
+				continue;
+			}
+			else if(array[i].equals("1") && array[i+1].equals("1")) {
+				continue;
+			}
+			else if(array[i].equals("1") && array[i+1].equals("0")) {
+				onetozero++;
+			}
+	}
+		int max = Math.max(onetozero, zerotoone);
+		
+		if(Math.abs(zerotoone-onetozero)== 1) {
+			result = max;
+		}
+		else {
+			if(zerotoone<onetozero) {
+				result = zerotoone;
+			}
+			else if(zerotoone==0 && onetozero==0) {
+				result =1;
+			}
+			else if(zerotoone == onetozero) {
+				result = onetozero;
+			}
+			else {
+				result = onetozero;
+			}
+		}
+		
+		// 하나는 0 하나는 2 일때 경우 추
+		
+		System.out.println(zerotoone);
+		System.out.println(onetozero);
+		System.out.println(result);
+}
+}
