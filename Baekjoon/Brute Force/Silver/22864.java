6e21ce6cf461f83f4a17f16d8103076f5ef6a568
Jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-06-30 03:09:12 +0900

    Update 22864.java

--- Baekjoon/Brute Force/Silver/22864.java
+++ Baekjoon/Brute Force/Silver/22864.java
@@ -1,1 +1,35 @@
-~
+package algorithm;
+
+import java.io.*;
+import java.util.*;
+
+public class Test { 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(br.readLine());
+
+		int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+		int hour = 0;
+		int task = 0;
+		int tired = 0;
+		while(hour!=24) {
+			if(tired+A<=M) {		
+				tired+=A;
+				task+=B;
+			}
+			else {
+				tired-=C;
+                if(tired<0){
+                    tired=0;
+                }
+			}
+			hour++;
+		}
+		System.out.println(task);
+	}
+}
+
