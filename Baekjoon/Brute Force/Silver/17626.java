bab05b4ba01976d6d5b973484f3b00712aad3ba0
Jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-07-04 14:04:25 +0900

    Update 17626.java

--- Baekjoon/Brute Force/Silver/17626.java
+++ Baekjoon/Brute Force/Silver/17626.java
@@ -12,7 +12,7 @@ public class Test {
 		int n = Integer.parseInt(br.readLine());
 		dp = new int[n + 1];
 		dp[0] = 0;
-		dp[1] = 1;
+		dp[1] = 1;				// 1 = 1 * 1 로 고정이므로 1개 
 	 
 		solve(n);
 	 
@@ -20,12 +20,12 @@ public class Test {
 	}
 	
 	static void solve(int n) {
-		for (int i = 2; i <= n; i++) {
-			int min = Integer.MAX_VALUE;
-			for (int j = 1; j * j <= i; j++) {
-				min = Math.min(min, dp[i - j * j]);
+		for (int i = 2; i <= n; i++) {				// for( 2<=i<=n )
+			int min = Integer.MAX_VALUE;			// 최솟값을 비교할 것이므로 아래 for문이 도는 처음에 값 설정
+			for (int j = 1; j * j <= i; j++) {	
+				min = Math.min(min, dp[i - j * j]);	// 앞에 만든 dp로 계속 끌고간다.
 			}
-			dp[i] = min + 1; 
+			dp[i] = min + 1; 				// 값의 최솟값을 dp배열 i번째 인덱스에 저장하고 뒤에서 계속 사용할 것
 		}
 	}
 }    	
